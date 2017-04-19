d0afd9a40f1efc3df5a1f84e84c749b72f4ade69
duryeong <duryeong@gmail.com>
2017-04-19 15:53:27 +0900

    로그인 기능 및
    포스트 상세 보기에서 글 수정 삭제 퍼블리시 버튼 작성자만 보이도록 반영

--- startdjango/settings.py
+++ startdjango/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from django.conf.global_settings import STATIC_ROOT
+from django.conf.global_settings import STATIC_ROOT, LOGIN_REDIRECT_URL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+LOGIN_REDIRECT_URL = "/"
 
 
 # Quick-start development settings - unsuitable for production
