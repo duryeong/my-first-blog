b2b86cac972a7c04994cc013702c0c58048bcbd9
duryeong <duryeong@gmail.com>
2017-04-19 16:34:00 +0900

    코멘트 추가 버스 해결

--- blog/views.py
+++ blog/views.py
@@ -69,11 +69,11 @@ def add_comment_to_post(request, pk):
    post = get_object_or_404(Post, pk=pk)
    if request.method == "POST":
       form = CommentForm(request.POST)
-   if form.is_valid():
-      comment = form.save(commit=False)
-      comment.post = post
-      comment.save()
-      return redirect('post_detail', pk=post.pk)
+      if form.is_valid():
+         comment = form.save(commit=False)
+         comment.post = post
+         comment.save()
+         return redirect('post_detail', pk=post.pk)
    else:
       form = CommentForm()
 
